5f3e041d6072e6d3168e8b534737bb54fed4bb6d
papatomatoe <hole_in_the_wall@list.ru>
2021-04-10 20:39:55 +0300

    add: images and build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const rename = require('gulp-rename');
 const csso = require('gulp-csso');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const svgstore = require('gulp-svgstore');
+const del = require('del');
 const browserSync = require('browser-sync').create();
 
 const css = () => {
@@ -19,19 +23,81 @@ const css = () => {
     .pipe(csso())
     .pipe(rename('style.min.css'))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('source/css'))
+    .pipe(gulp.dest('build/css'))
     .pipe(browserSync.stream());
+};
+
+const images = () => {
+  return gulp.src('source/images/**/*.{jpg,png}')
+    .pipe(imagemin([
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+      imagemin.mozjpeg({
+        quality: 75,
+        progressive: true
+      })
+    ]))
+    .pipe(gulp.dest('source/tempImages'))
+    .pipe(webp({
+      quality: 75,
+      preset: "photo"
+    }))
+    .pipe(gulp.dest('source/tempImages'))
+};
+
+const sprite = () => {
+  return gulp.src('source/images/**/*.svg')
+    .pipe(imagemin([
+      imagemin.svgo()
+    ]))
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('source/tempImages'))
+};
+
+const copy = () => {
+  gulp.src('source/fonts/*.{woff,woff2}')
+    .pipe(gulp.dest('build/fonts'));
+
+  gulp.src('source/js/*.js')
+    .pipe(gulp.dest('build/scripts'));
+
+  gulp.src('source/lib/*.css')
+    .pipe(csso())
+    .pipe(gulp.dest('build/css'));
+
+  gulp.src('source/tempImages/*.{jpg,png,webp,svg}')
+    .pipe(gulp.dest('build/images'));
+
+  gulp.src('source/icons/*')
+    .pipe(gulp.dest('build/icons'));
+
+  return gulp.src('source/*.html')
+    .pipe(gulp.dest('build/'));
+}
+
+const cleanBuild = () => {
+  return del('build');
+};
+
+const cleanTemp = () => {
+  return del('source/tempImages');
 };
 
 const server = () => {
   browserSync.init({
     server: {
-      baseDir: 'source/'
+      baseDir: 'build/'
     }
   });
 
   gulp.watch('source/sass/**/*.{sass,scss}', gulp.series(css));
   gulp.watch('source/*.html').on('change', browserSync.reload);
-}
+};
 
-exports.start = gulp.series(css, server);
+exports.build = gulp.series(cleanBuild, css, copy);
+exports.start = gulp.series(cleanBuild, css, copy, server);
+exports.images = gulp.series(cleanTemp, gulp.parallel(images, sprite));
